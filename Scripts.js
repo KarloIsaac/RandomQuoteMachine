1e8b56f1ee41b9d6d56b871648587890b0e3866b
KarloIsaac <qafqunam2001@gmail.com>
2017-11-13 09:00:08 -0600

    The Twitter button sends the quote info to the Twitter web intent. The request quote function is called on the onloand event of the window.

--- Scripts.js
+++ Scripts.js
@@ -9,7 +9,17 @@ function requestQuote() {
     var jsonpRequesterScript = document.getElementById("jsonpRequester");
     document.body.removeChild(jsonpRequesterScript);
     jsonpRequesterScript = document.createElement("script");
-    jsonpRequesterScript["src"] = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=mycallback";
+    jsonpRequesterScript["src"] = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=mycallback";
     jsonpRequesterScript.id = "jsonpRequester";
     document.body.appendChild(jsonpRequesterScript);
+}
+
+
+function postToTwitter() {
+    var twitterWebIntentUrl = "https://twitter.com/intent/tweet?text=";
+    var quote = document.getElementById("quote_holder").textContent;
+    var author = document.getElementById("author").innerHTML;
+    twitterWebIntentUrl = twitterWebIntentUrl + quote + author;
+    twitterWebIntentUrl = encodeURI(twitterWebIntentUrl);
+    window.open(twitterWebIntentUrl);
 }
